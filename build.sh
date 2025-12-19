192c00f26aa304c2974c5e0681df8a5d09495b22
o137 <git@o137.net>
2025-12-19 15:32:26 +0900

    feat: cron

--- build.sh
+++ build.sh
@@ -5,13 +5,23 @@
 
 set -euo pipefail
 
+KERNEL_TAG="${1:-}"
+ZFS_TAG="${2:-}"
+
+# Fallback for manual execution
+if [ -z "$KERNEL_TAG" ]; then
+  KERNEL_TAG="$(curl -s https://api.github.com/repos/microsoft/WSL2-Linux-Kernel/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)"
+  # Or, use the kernel version of the current environment
+  # KERNEL_TAG="linux-msft-wsl-$(uname -r | cut -d '-' -f1)"
+fi
+
+if [ -z "$ZFS_TAG" ]; then
+  ZFS_TAG="$(curl -s https://api.github.com/repos/openzfs/zfs/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)"
+fi
+
 ROOT_DIR="$(pwd)/.tmp"
 KERNEL_REPO="https://github.com/microsoft/WSL2-Linux-Kernel.git"
-KERNEL_TAG="$(curl -s https://api.github.com/repos/microsoft/WSL2-Linux-Kernel/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)"
-# KERNEL_TAG="linux-msft-wsl-$(uname -r | cut -d '-' -f1)"
-
 ZFS_REPO="https://github.com/openzfs/zfs.git"
-ZFS_TAG="$(curl -s https://api.github.com/repos/openzfs/zfs/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)"
 
 # ensure_repo <repo_url> <tag> <destination>
 ensure_repo() {
@@ -27,7 +37,6 @@ ensure_repo() {
   )
 }
 
-# TODO: lock with commit hash
 ensure_repo "$ZFS_REPO" "$ZFS_TAG" "$ROOT_DIR/zfs"
 ensure_repo "$KERNEL_REPO" "$KERNEL_TAG" "$ROOT_DIR/kernel"
 
@@ -41,17 +50,12 @@ ensure_repo "$KERNEL_REPO" "$KERNEL_TAG" "$ROOT_DIR/kernel"
   # Or, use configuration from current environment
   # cat /proc/config.gz | gunzip > .config
 
-  # Apply new options with defaults (non-interactive)
+  # Apply new options with defaults
   make olddefconfig
 
-  # Prepare the kernel
+  # Prepare the kernel (Module.symvers is needed for ZFS)
   # TODO: we probably only need to run some targets
-  # make -j"$(nproc)" modules_prepare
   make -j"$(nproc)" modules
-
-  # Fake Modules.symvers file to satisfy ZFS build system
-  # TODO: should we actually generate this by `make modules`?
-  touch Module.symvers
 )
 
 # Build ZFS kernel module
